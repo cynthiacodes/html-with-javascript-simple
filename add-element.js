45c719508b31360d113a56129d4a9ed9a6b020bc
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-27 15:26:54 +0100

    added new element

--- add-element.js
+++ add-element.js
@@ -10,3 +10,16 @@ newDiv.appendChild(newContent);
 // add the newly created element and its content into the DOM
 const existingDiv = document.getElementById("my-target-element");
 document.body.appendChild(newDiv, existingDiv);
+
+// create a new div element
+const newH2 = document.createElement("h2");
+
+// and give it some content
+const newContentForH2 = document.createTextNode("Hi there!");
+
+// add the text node to the newly created div
+newH2.appendChild(newContentForH2);
+
+// add the newly created element and its content into the DOM
+const existingH2 = document.getElementById("my-target-element2");
+document.body.appendChild(newH2, existingH2);
